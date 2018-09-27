7800c494ed50eb6813a4a1d5b23b7c865a67b5c4
Anatoliy Lobach <anatoliylobach@MBP-Anatoliy.Dlink>
2018-09-28 00:34:34 +0300

    добавил компонент для навигации

---
+++ src/components/navigation.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="menu-navigation">
+      <a :href="link.link" v-for="link in links" :key="link.text">
+        {{ link.text }}
+      </a>
+
+
+      <span @click="open">&#9776;</span>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'menuNavigation',
+  data() {
+    return {
+      links: [
+        {
+          text: 'Главная',
+          link: '#'
+        },
+        {
+          text: 'Витрина',
+          link: '#'
+        },
+        {
+          text: 'Контакты',
+          link: '#'
+        },
+        {
+          text: 'О нас',
+          link: '#'
+        },
+        {
+          text: 'Купить оптом',
+          link: '#'
+        }
+      ]
+    };
+  },
+  methods: {
+    open() {
+      console.log('Open menu');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.menu-navigation a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+</style>
